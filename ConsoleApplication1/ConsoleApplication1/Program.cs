0f8609f17030c5a8e6756baa63af3261270d7c35
ESchulstad <eschulstad@gmail.com>
2016-11-03 16:13:44 -0400

    Added Hi There

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Things");
+            Console.WriteLine("Hi There!");
             Console.ReadKey();
         }
     }
